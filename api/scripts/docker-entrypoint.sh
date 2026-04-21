2ca96587ce935db8f6b89a9d04ae6d02f7674bb4
Eduard Malokhvii <malokhvii.ee@gmail.com>
2026-04-21 23:24:56 +0300

    refactor(docker): split migrations into dedicated service

--- api/scripts/docker-entrypoint.sh
+++ api/scripts/docker-entrypoint.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env sh
 set -e
 
-main() {
-  if [ "$DJANGO_COLLECTSTATIC" = true ]; then
-    python manage.py collectstatic -v 2 --noinput
-  fi
+if [ "$DJANGO_COLLECTSTATIC" = true ]; then
+  python manage.py collectstatic -v 2 --noinput
+fi
 
-  if [ "$DJANGO_MIGRATE" = true ]; then
-    python manage.py migrate --noinput
-  fi
+if [ -n "$SUPERUSER_USERNAME" ] && [ -n "$SUPERUSER_EMAIL" ] &&
+  [ -n "$SUPERUSER_PASSWORD" ]; then
+  python manage.py safecreatesuperuser \
+    --username "$SUPERUSER_USERNAME" \
+    --email "$SUPERUSER_EMAIL" \
+    --password "$SUPERUSER_PASSWORD"
+fi
 
-  if [ -n "$SUPERUSER_USERNAME" ] && [ -n "$SUPERUSER_EMAIL" ] &&
-    [ -n "$SUPERUSER_PASSWORD" ]; then
-    python manage.py safecreatesuperuser \
-      --username "$SUPERUSER_USERNAME" \
-      --email "$SUPERUSER_EMAIL" \
-      --password "$SUPERUSER_PASSWORD"
-  fi
-
-  exec gunicorn "$@"
-}
-
-main "$@"
+exec gunicorn "$@"
